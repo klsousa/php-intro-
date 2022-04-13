528010f252cf6157e8ad0d409b252c264ad0e5eb
kelvin <ks796198@gmail.com>
2022-04-13 14:44:32 -0300

    Foi adicionado a categoria idoso

--- index.php
+++ index.php
@@ -25,13 +25,18 @@ else if($idade >= 13 && $idade <= 18){
     if($categoria[$i] == 'adolescente')
     echo 'O nadador'.$nome.'compete na categoria adolescente';
     }
-}
-else{
+else if ($idade >= 19 && $idade <= 59){
     for($i = 0; $i <= count($categoria); $i++)
     {
     if($categoria[$i] == 'adulto')
     echo 'O nadador'.$nome.'compete na categoria adulto';
     }
 }
-
+else {
+    for($i = 0; $i <= count($categoria); $i++)
+    {
+    if($categoria[$i] == 'idoso')
+    echo 'O nadador'.$nome.'compete na categoria idoso';
+    }
+}  
 ?>
